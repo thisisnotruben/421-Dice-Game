4b01a5256f1e37d5b0d45fe89185cf91ce99d7de
Ruben Alvarez Reyes <rubenar1996@gmail.com>
2020-05-02 13:45:28 -0700

    added roll functionality

--- MainScreenController.java
+++ MainScreenController.java
@@ -12,17 +12,20 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.TextFlow;
 
+import java.util.Random;
+
 
 public class MainScreenController {
 
     private final static String imgPath = "asset\\img\\%s.png";
-    private final static Image dieImages[] = {
+    private final static Image[] dieImages = {
             new Image(String.format(imgPath, 1)),
             new Image(String.format(imgPath, 2)),
             new Image(String.format(imgPath, 3)),
             new Image(String.format(imgPath, 4)),
             new Image(String.format(imgPath, 5)),
-            new Image(String.format(imgPath, 6))
+            new Image(String.format(imgPath, 6)),
+            new Image(String.format(imgPath, 7))
     };
 
     private MainScreenModel model;
@@ -32,7 +35,11 @@ public class MainScreenController {
     @FXML
     private Label player1ScoreLabel;
     @FXML
+    private Label player1RollLabel;
+    @FXML
     private Label player2ScoreLabel;
+    @FXML
+    private Label player2RollLabel;
     @FXML
     private ImageView die1Image;
     @FXML
@@ -56,17 +63,53 @@ public class MainScreenController {
 
     @FXML
     public void initialize() {
-
+        newTurn();
     }
 
     @FXML
     public void onRollPressed() {
-        System.out.println("Roll pressed");
+//        set roll counter text
+        int playerTurn = model.getPlayerTurn();
+        String rollText = String.format("Roll: %d", model.getCurrentPlayerRollCount() + 1);
+        String defaultRollText = "Roll: 0";
+        if (playerTurn == 0) {
+            player1RollLabel.setText(rollText);
+            player2RollLabel.setText(defaultRollText);
+        } else {
+            player2RollLabel.setText(rollText);
+            player1RollLabel.setText(defaultRollText);
+        }
+//        roll die
+        model.setDie(rollDie(), rollDie(), rollDie());
+        if (playerTurn == model.getPlayerTurn() && model.getDie(0) > 0 && model.getDie(1) > 0 && model.getDie(2) > 0) {
+//          set die label text
+            dieStatusLabel.setText(String.format("%d-%d-%d", model.getDie(0), model.getDie(1), model.getDie(2)));
+//          set die images
+            die1Image.setImage(dieImages[model.getDie(0) - 1]);
+            die2Image.setImage(dieImages[model.getDie(1) - 1]);
+            die3Image.setImage(dieImages[model.getDie(2) - 1]);
+        } else {
+            newTurn();
+        }
     }
 
     @FXML
     public void onDonePressed() {
-        System.out.println("Done pressed");
+        model.switchPlayers();
+        newTurn();
+    }
+
+    private int rollDie() {
+//        return a random number from 1-6
+        return new Random().nextInt(6) + 1;
+    }
+
+    private void newTurn() {
+        dieStatusLabel.setText("Roll the die!");
+        playerTurnLabel.setText(String.format("Player turn: %d", model.getPlayerTurn() + 1));
+        die1Image.setImage(dieImages[6]);
+        die2Image.setImage(dieImages[6]);
+        die3Image.setImage(dieImages[6]);
     }
 
 }
